a1e76c00af37d955b0ea2a648e3381fa645de085
iizukamega <ta-matsuarc@mineo.jp>
2026-09-14 14:52:38 +0900

    add home

---
+++ hakkutu/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <div className="home">
+      <h1>いい感じの文章</h1>
+      <button type="button" className="capture-button">
+        撮影
+      </button>
+    </div>
+  )
+}
+
+export default Home
